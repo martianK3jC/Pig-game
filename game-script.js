d4934cf48a273652e297ae7072754a1b3d7bef9d
martianK3jC <kceniza24@gmail.com>
2025-07-10 22:48:09 +0800

    update

--- game-script.js
+++ game-script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');//alternative way for id
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+const diceEl = document.querySelector('.dice');
+
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+//Display dice and score
+btnRoll.addEventListener('click', function(){
+    let dice = Math.trunc(Math.random() * 6 + 1);
+    diceEl.classList.remove('hidden');
+    diceEl.scr = `DIE${dice}.png`;
+
+    if(dice !== 1){
+        //Add dice to current  score
+        currentScore += dice;
+    }
+})
